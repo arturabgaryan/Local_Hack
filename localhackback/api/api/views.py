41eb8aa566a42a035dd56ce8da8e542e75895911
GlowDanBios <46843591+GlowDanBios@users.noreply.github.com>
2020-04-19 16:54:57 +0300

    Add files via upload

--- localhackback/api/api/views.py
+++ localhackback/api/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.forms.models import model_to_dict
 # Create your views here.
 import json
 import pandas as pd
@@ -6,18 +7,18 @@ from .models import Day
 
 
 def cases(request):
-    df = pd.read_csv('/home/artur/Desktop/Local_Hack_files/Local_Hack/Cases.csv')
+    df = pd.read_csv('C:\\Users\\hp\\Downloads\\cases.csv')
     data = {}
-    for col in df.columns:
-        if 'Unnamed' not in col:
-            data[col] = []
-            for i in range(len(df)):
-                data[col].append({'time': df['Unnamed: 0'][i], 'cases': int(df[col][i])})
+    obj = Day.objects.all()
+    for o in obj:
+        if o.name not in data.keys():
+            data[o.name] = []
+        data[o.name].append({'time': str(o.date), 'cases': o.cases})
     return HttpResponse(json.dumps(data), status=200)
 
 
 def deaths(request):
-    df = pd.read_csv('/home/artur/Desktop/Local_Hack_files/Local_Hack/Deaths.csv' )
+    df = pd.read_csv('C:\\Users\\hp\\Downloads\\deaths.csv')
     data = {}
     for col in df.columns:
         if 'Unnamed' not in col:
@@ -28,7 +29,7 @@ def deaths(request):
 
 
 def recoveries(request):
-    df = pd.read_csv('/home/artur/Desktop/Local_Hack_files/Local_Hack/Recovery.csv')
+    df = pd.read_csv('C:\\Users\\hp\\Downloads\\recovery.csv')
     data = {}
     for col in df.columns:
         if 'Unnamed' not in col:
@@ -37,14 +38,10 @@ def recoveries(request):
                 data[col].append({'time': df['Unnamed: 0'][i], 'cases': int(df[col][i])})
     return HttpResponse(json.dumps(data), status=200)
 
-def graph(request,day_id):
-    country_name = get_object_or_404(Day, pk=day_id)
-    countries = Day.objects.all().filter(name=country_name)
-    for i in countries:
-        print(i.deaths,i.recoveries,i.cases)
 
 def index(request):
     return render(request, 'index.html')
+
 
 #
 # def bd(request):
@@ -61,3 +58,27 @@ def index(request):
 #                 a.recoveries = recoveries[col][i]
 #                 a.date = cases['Unnamed: 0'][i]
 #                 a.save()
+
+
+def graph(request):
+    name = request.GET.get('country', None)
+    day = request.GET.get('day', None)
+    countries = Day.objects.filter(name=name)
+    data = []
+    for i in range(int(day)):
+        data.append({'cases': countries[i].cases, 'deaths': countries[i].deaths, 'recoveries': countries[i].recoveries})
+
+    return HttpResponse(status=200, content=json.dumps(data))
+
+
+def predict(request):
+    df = pd.read_csv('C:\\Users\\hp\\Downloads\\predictions.csv')
+    cases = df[0:7]
+    deaths = df[7:14]
+    recoveries = df[14:21]
+    data = []
+    for i in range(7):
+        for col in cases.columns:
+            if 'Unnamed' not in col:
+                data.append([col, int(cases[col][i]), int(deaths[col][i + 7]), int(recoveries[col][i + 14])])
+    return HttpResponse(status=200, content=json.dumps(data))
